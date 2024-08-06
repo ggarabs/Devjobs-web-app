2eba1cac29c17e406f2b3a024a445c0a67ded792
gustavo <ggarabetti0607@gmail.com>
2024-08-05 21:48:38 -0300

    bug: searchbox dark mode styles

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -16,8 +16,27 @@ const SearchBox: React.FC = () => {
 
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.container}>
-                <input type="text" ref={inputRef} placeholder='Filter by Title...' className={`${styles.input} ${themeClass}`} />
+            <div className={`${styles.container} ${themeClass}`}>
+                <div className={`${styles.titleContainer} ${themeClass}`}>
+                    <div className={styles.magnifierContainer}>
+                        <img src="blueMagnifier.png" className={styles.magnifierImg} />
+                    </div>
+                    <input type="text" ref={inputRef} placeholder='Filter by title...' className={`${styles.input} ${themeClass}`} />
+                </div>
+                <div className={`${styles.locationContainer} ${themeClass}`}>
+                    <div className={styles.pinContainer}>
+                        <img src="location.png" className={styles.pinImg} />
+                    </div>
+                    <input type="text" ref={inputRef} placeholder='Filter by location...' className={`${styles.input} ${themeClass}`} />
+                </div>
+                <div className={styles.fullTimeModeContainer}>
+                    <label htmlFor="fullTime" className={styles.fullTimeCheckboxContainer}>
+                        <div className={styles.checkImgContainer}>
+                            <input type="checkbox" id="fullTime" ref={inputRef} placeholder='Filter by Title...' className={`${styles.checkbox} ${themeClass}`} />
+                        </div>
+                        <p className={styles.label}>Full Time</p>
+                    </label>
+                </div>
                 <img src={filterImg} className={styles.filter} />
                 <button type="submit" className={styles.button}>
                     <img src="magnifier.png" className={styles.img} />
